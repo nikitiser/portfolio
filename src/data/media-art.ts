d1d04b30735eda3fd84f10ae1825c0175e963baf
the-sociophobic <torikowashi@gmail.com>
2021-03-16 15:02:39 +0300

    upd links: new media art, photography

--- src/data/media-art.ts
+++ src/data/media-art.ts
@@ -7,33 +7,33 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "https://psv4.userapi.com/c532036/u63043058/docs/d5/7194cee97b50/Human_design.png?extra=ioqJJmdRY4TlIA3lnkqC0Dm1x-jzn-ZjZ73F3rRMETAOQVOAxyUqepyQU1qn7aQig-Ir5QhFkNUImsL2lOXiJd8-JvBL69E4Ne5t53WmeWffic0MzBd3v4yGRY7mrlpPA6SCD5txBpaJ5ONMFUfWKxLE",
-        autoplay: true,
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/1.jpg",
+        autoplay: false,
       },
 
     ],
   },
-{
-  title: "Human design new era (2020-2021)",
-  description: "Human design new era — c’est une exposition virtuelle interactive. Ce projet a été réalisé dans UNITY Mes animations répresentent les objets exposés dans le foret. Sur le site-web il y a la description complete Human design new era",
-  blocks: [
-    {
-    link:"https://youtu.be/UvlHIiF-hJQ",
-    autoplay: false,
+  {
+    title: "Human design new era (2020-2021)",
+    description: "Human design new era — c’est une exposition virtuelle interactive. Ce projet a été réalisé dans UNITY Mes animations répresentent les objets exposés dans le foret. Sur le site-web il y a la description complete Human design new era",
+    blocks: [
+      {
+        link: "https://youtu.be/UvlHIiF-hJQ",
+        autoplay: false,
+      },
+    ],
   },
-  ],
-},
-{
-  title: "",
-  description: "",
-  blocks: [
-    {
-      link: "https://psv4.userapi.com/c536436/u63043058/docs/d34/600d5af2641f/Snimok_ekrana_2021-03-09_v_11_52_25.png?extra=-89mPPA64U1_fp3LdnpsMtDKm6G70QaeGd-Ugx0dqbrOVcw61TNeRT0wbpdKVXdA-WikeYHIGz9Qb_BSB44wRBneAg81oBnhiFzRJswsrrpx-k8sRCf37bqb6_kXBw1eOfKiBnjl9vlMdJ_ok1rUV4Jj",
-      autoplay: false,
-    },
-
-  ],
-},
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/2.jpg",
+        autoplay: false,
+      },
+  
+    ],
+  },
   {
     title: "Halo",
     description: "hiver 2021, performance audiovisuelle. Alexander Kozlov, Marta Shilova, Svetlana Budikina. La nouvelle scène du Théâtre Alexandrinsky dans le cadre du concert dédié au 5e anniversaire du Laboratoire des Nouveaux Médias. Créez des graphiques audio réactifs inspirés du phénomène optique naturel: le halo. ",
@@ -50,9 +50,10 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "https://psv4.userapi.com/c520036/u63043058/docs/d48/f68345ba8c83/Snimok_ekrana_2021-03-09_v_12_01_17.png?extra=eGhczCdK4u_DYeB0QGQnPNAdPA512Vq7UEdS-BekadQYChYBDATiUUrN5BpJJmETyHC5ioJ7fphI3lW1NOrk9n6t20Co6zPxhyjSJbJ9VtM64RRt9FMQLeFJY7ZB_4O0vSWpJ9ArreRrkPiZjwZCUs89",
-        autoplay: true,
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/3.jpg",
+        autoplay: false,
       },
+  
     ],
   },
   {
@@ -63,6 +64,17 @@ const data: Project[] = [
         link: "https://youtu.be/GWq2IeNa9ro",
         autoplay: false,
       },
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/4.jpg",
+        autoplay: false,
+      },
+  
     ],
   },
   {
@@ -80,10 +92,10 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: " https://psv4.userapi.com/c536132/u63043058/docs/d24/e33bb848fac4/Snimok_ekrana_2021-03-09_v_13_28_02.png?extra=mX0jiJlyeWytPHlHy_0QqrXPhl4n5x1AvVfRQxovfcD3CaBoSQ2MNzvK7tzhEkZ7vhfOaZCIfDLOTVYpMrRXBX7tRRU4u2l2r7Vzc1OedRTF0BgijvG_26J-T9a3_zxyuHSWgJtmlW3gKZTdFccY3KNz",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/5.jpg",
         autoplay: false,
       },
-
+  
     ],
   },
   {
@@ -101,20 +113,14 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "https://psv4.userapi.com/c520036/u63043058/docs/d51/564194bec145/Snimok_ekrana_2021-03-09_v_19_12_48.png?extra=2m2-3T5dABRz-nHBYzVKx4d743Z3_4U_M_0jbAo-VR9-xQPRuVVKfZHpCVTo7Bz--8Aeek8IgKmzcoYoZAg7tFEixWUzyUytBJ4n2HoHV-Go_cu-w5m6njIiz-xjzlANDbU-evJwIczI21iGbop5cTNa",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/6.jpg",
         autoplay: false,
       },
-    ]
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
       {
-        link: "https://psv4.userapi.com/c520036/u63043058/docs/d38/5fee416acc50/Snimok_ekrana_2021-03-09_v_19_14_43.png?extra=CKbeWhQMQ8TppRZhN9c9LuPp9qA9xH5vBbMJZ27rVgRLb2RDsVjbfHtnQybBz2bQ86j5pZ2vDNYsdItA483iSGpDt4K2EPpDhVkv3W6ftOIv1OCFAbDLSTf9S6qcZbPV9eiq8BlITP4Y8U3jTATvCc1m",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/new%20media%20art/7.jpg",
         autoplay: false,
-      },
-    ]
+      },  
+    ],
   },
   {
     title: "Jeu d’échecs. forme+visualisation (2018-2019)",
--- src/data/photography.ts
+++ src/data/photography.ts
@@ -3,14 +3,13 @@ import { Project } from '../types'
 
 const data: Project[] = [
   {
-    title: "",
+    title: "Portraits",
     description: "",
     blocks: [
       {
-        link: "",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/1.png",
         autoplay: true,
       },
-
     ],
   },
   {
@@ -18,85 +17,29 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d38/dcdd4acea6bb/Snimok_ekrana_2021-03-09_v_17_05_43.png?extra=Ko4ma0gyhq6YQJSrQbuQ_UrtEf81927hlH27pOCJMCPj0kq_VgW9RZy7P8s-bpueABl3CmYmYmueqwwpzNSDm8eP5UMPUUJX2otVURfdCJC7hdtRbSscfQkL7L8NszsZt-GJPkzhQK_adgTSfSc-bSeW",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d35/21062d3b1997/Snimok_ekrana_2021-03-09_v_17_07_14.png?extra=FeZYaEAgCHeyoFlzoU9j6gkQPctRn-3nQ591z3YU91UWsWj8RDzjwAIo2UGtoGxmOy9xRlXE2GvpUuLg5dFuEBHn1GFcbOAYLQ_K_WwxwwDFXFTNas292qKNVoDiMIpwPDlbJpK-TGzLf_cYKOtlIKXQ",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/2.jpg",
         autoplay: true,
       },
     ],
   },
   {
-    title: "",
+    title: "Portraits",
     description: "",
     blocks: [
       {
-        link: "",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/3.jpg",
         autoplay: true,
       },
-
     ],
   },
   {
-    title: "",
+    title: "Naturemortes photographiques",
     description: "",
     blocks: [
       {
-        link: "",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/4.jpg",
         autoplay: true,
       },
-
-    ],
-  },
-  {
-    title: "Natures mortes photographiques",
-    description: "",
-    blocks: [
-      {
-        link: "",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d27/3aad015fa7bd/Snimok_ekrana_2021-03-09_v_17_07_37.png?extra=-axzV3HuZAwwsRzlwiTPYq0PS9VXthwiBc0CT4kI4neYf_BlhAnopO2VX5DCpM82un9Tk5G0J013Ij_hPPpaHcaGtIjhSIDiuQj32D7MY73gEgXgu2mi_YRfSrIa_yok1OUdR910bLBeOapdqNARW1P7",
-        autoplay: true,
-      },
-    ]
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "",
-        autoplay: true,
-      },
-
     ],
   },
   {
@@ -104,43 +47,19 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/5.jpg",
         autoplay: true,
       },
-
     ],
   },
   {
-    title: "",
+    title: "Ville",
     description: "",
     blocks: [
       {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d47/a63013ad041e/Snimok_ekrana_2021-03-09_v_17_07_47.png?extra=mH5KUFFTkX8Igt3d_u4GIsndNYfmhRDgB3pPw_QMcdMFRhiiRoJ4ELx6iymYMYqLmWCITzDesXwFxesqk01jPhSZmjIvVQvAaLpPvQSu5i2URdp5o7X_OPlknra_a9WYTpGBCZ7YdWhn8g3hdbrQL1pk",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/6.jpg",
         autoplay: true,
       },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d28/05306255ee6e/Snimok_ekrana_2021-03-09_v_17_07_03.png?extra=VKEXs40dP_A7iQsUmQe6nLD2iDa9xey6cNRtTqEHTp1vl6_AfWKlGY8vtif9VYgAaEMjkXkLZJluzLRlL8XWhKqGXNgN6dYSjIwLwPk19P1MZ75tKucRf9QAcc683UfUT37FqnDOq6dKFJtpWS_8zmkx",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "",
-        autoplay: true,
-      },
-
     ],
   },
   {
@@ -148,29 +67,17 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/7.jpg",
         autoplay: true,
       },
-
     ],
   },
   {
-    title: "",
+    title: "Photographique",
     description: "",
     blocks: [
       {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d23/b4396bfa570a/Snimok_ekrana_2021-03-09_v_17_06_21.png?extra=1vlVUT6Qe4TqfTVTVWgWIUeP4SadgbUsvZIZVQUk9_CJ2kxOdSva15RscUfaxIHEZbNovemP0MuteGeZp15yaoI-uOWQqyfBG4axdR1X8mWbEu1b8SJ-ZentSJ9JYbBAkuzpVgUWrPc4wJROUntYL8oW",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "",
-    description: "",
-    blocks: [
-      {
-        link: "https://psv4.userapi.com/c534536/u63043058/docs/d26/2a93230e1213/Snimok_ekrana_2021-03-09_v_17_06_44.png?extra=WmIW8B7Le_QGbomSsN2Y2byDd_fzQYJLdP4gxUmYa67ZvWiJEprG1cQF_GrYiVxiwzERzlfMut4FHDZG4YfuEVkrVlevMR64dhVFDT0z9ctx8iibF8PvoRDgRADtMfResm2WKtYT0VQtfmyL9sk16h6L",
+        link: "https://storage.yandexcloud.net/cdn.tochkadostupa.spb.ru/the_sociophobic/smarta.tech/photography/8.jpg",
         autoplay: true,
       },
     ],
